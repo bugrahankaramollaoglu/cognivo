a3558969e3673dd8e843c5726d13e53a705ad616
buğrahan karamollaoğlu <bugrahankaramollaoglu@gmail.com>
2025-08-14 19:23:52 +0300

    OK. initial commit for a fresh start.

--- android/app/src/main/kotlin/com/example/cognivo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cognivo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:ui';
-
-class AppColors {
-  static final customGrey = Color(0xFF0C0F14);
-  static final customGreen = Color(0xFFBCFE4B);
-}
